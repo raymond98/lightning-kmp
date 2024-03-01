e3076602d30733df11dd3a4061c34e288ba2bd35
t-bast <bastien@acinq.fr>
2024-03-01 17:53:30 +0100

    Remove `please_open_channel`
    It is usually the wallet that decides that it needs a channel, but we
    want the LSP to pay the commit fees to allow the wallet user to empty
    its wallet over lightning. We previously used a `please_open_channel`
    message that was sent by the wallet to the LSP, but it doesn't work
    well with liquidity ads.
    
    We remove that message and instead send `open_channel` from the wallet
    but with a custom channel flag that tells the LSP that they should be
    paying the commit fees. This only works if the LSP adds funds on their
    side of the channel, so we couple that with liquidity ads to request
    funds from the LSP.
    
    We also add a `recommended_feerates` message from the LSP which lets
    the wallet know the on-chain feerates that the LSP will accept for
    on-chain funding operations, since those feerates are set in the
    `open_channel` message that is now sent by the wallet.

--- src/commonMain/kotlin/fr/acinq/lightning/json/JsonSerializers.kt
+++ src/commonMain/kotlin/fr/acinq/lightning/json/JsonSerializers.kt
@@ -66,6 +66,7 @@
     JsonSerializers.LiquidityLeaseFeesSerializer::class,
     JsonSerializers.LiquidityLeaseWitnessSerializer::class,
     JsonSerializers.LiquidityLeaseSerializer::class,
+    JsonSerializers.ChannelFlagsSerializer::class,
     JsonSerializers.ChannelParamsSerializer::class,
     JsonSerializers.ChannelOriginSerializer::class,
     JsonSerializers.CommitmentChangesSerializer::class,
@@ -295,6 +296,9 @@ object JsonSerializers {
 
     @Serializer(forClass = LiquidityAds.Lease::class)
     object LiquidityLeaseSerializer
+
+    @Serializer(forClass = ChannelFlags::class)
+    object ChannelFlagsSerializer
 
     @Serializer(forClass = ChannelParams::class)
     object ChannelParamsSerializer
